6ca4e4c32452c79c7e06e94c8584302aa0746189
Yahyr Paredes <yahyrparedesarteaga@gmail.com>
2022-03-04 11:44:55 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
@@ -16,19 +16,18 @@ import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      {/* add routes with layouts */}
-      <Route path="/admin" component={Admin} />
-      <Route path="/auth" component={Auth} />
-      {/* add routes without layouts */}
-      <Route path="/landing" exact component={Landing} />
-      <Route path="/profile" exact component={Profile} />
-      {/*<Route path="/" exact component={Index} />*/}
-      {/*/!* add redirect for first page *!/*/}
-      <Redirect from="*" to="/auth" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            <Route path="/admin" component={Admin}/>
+            <Route path="/auth" component={Auth}/>
+            {/* add routes without layouts */}
+            <Route path="/" exact component={Landing}/>
+            <Route path="/profile" exact component={Profile}/>
+            {/*<Route path="/" exact component={Index} />*/}
+            <Redirect from="*" to="/"/>
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
